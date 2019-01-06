5eb62b7dca6f3e4ec48e2836e0e5d8d85f5b1a46
Tratt <imoxus@gmail.com>
2019-01-06 04:50:27 +0000

    adapted to new environment

--- matchmaking/views.py
+++ matchmaking/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
+
+from dawnotc import classes
+
+
 
 def index(request):
+	player = classes.Player("name")
+	return HttpResponse(str(player))
 	return HttpResponse("Hello. I am an offworld matchmaker.")
--- tournamentstatus/views.py
+++ tournamentstatus/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello. Look all this tournament status!")
+	return HttpResponse('Site under construction<br> <a href="https://github.com/Rhahi/MartianEnterpriseTournament/blob/master/design-document.txt"> Martian Enterprise Design Document </a>')

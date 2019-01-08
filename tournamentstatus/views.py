31f03131eeea375085efbd10d82d434c2ca209b9
Tratt <imoxus@gmail.com>
2019-01-08 16:47:01 +0000

    new temp webpage

--- tournamentstatus/views.py
+++ tournamentstatus/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse('Site under construction<br> <a href="https://github.com/Rhahi/MartianEnterpriseTournament/blob/master/design-document.txt"> Martian Enterprise Design Document </a>')
+	return HttpResponse('Site under construction<br> <a href="https://github.com/Rhahi/MartianEnterpriseTournament/blob/master/design-document.txt"> Martian Enterprise Design Document </a><br>Sign up by posting in #martian-enterprise in <a href="https://discord.gg/0pQ0rgV4DDFxO2rD">the official discord server</a>')

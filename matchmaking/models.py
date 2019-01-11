84f65c28ebf0e983234f857e581111422701c01e
Trattpingvin <imoxus@gmail.com>
2019-01-11 02:39:56 +0100

    fix syntax errors in models

--- matchmaking/models.py
+++ matchmaking/models.py
@@ -7,13 +7,13 @@ def validate_preference(val):
 #preference is 3 bit value. bit order 0x1 = mars, 0x10 = ceres, 0x100 = io
 #bit 1 means veto
 	if val<0 or val>7:
-		raise ValidationError("%(val)s is not a valid preference", params={'value':val])
+		raise ValidationError("%(val)s is not a valid preference", params={'value':val})
 
 def validate_availability(val):
 #availability is 4 bit value(one per day) bit order 0x1 = day 1, 0x10 = day 2, 0x100 = day 3, 0x1000 = day 4
 #bit 1 means available, 0 means unavailable
 	if val<0 or val>15:
-		raise ValidationError("%(val)s is not a valid availability", params={'value':val])
+		raise ValidationError("%(val)s is not a valid availability", params={'value':val})
 
 class Team(models.Model):
 	name = models.CharField(max_length = 128)
@@ -30,12 +30,12 @@ class Player(models.Model):
 	bracket = models.IntegerField()
 	stars = models.IntegerField()
 	preference = models.IntegerField(validators = [validate_preference])
-	availability = models.InteferField(validators = [validate_availability])
+	availability = models.IntegerField(validators = [validate_availability])
 
 class Match(models.Model):
 	LOCATIONS = [("M","Mars"), ("C", "Ceres"), ("I", "Io")]
 	location = models.CharField(choices=LOCATIONS, max_length = 6)
-	result = models.ForeignKey(
+	#result = models.ForeignKey(
 	class Meta:
 		abstract=True
 
@@ -49,7 +49,7 @@ class FFaMatch(models.Model):
 
 class Award(models.Model):
 	player = models.ForeignKey(Player, on_delete=models.CASCADE)
-	match = models.ForeignKey(Match, on_delete=models.CASCADE)
-	AWARDS = ["A", "award name"]
+	match = models.ForeignKey(FFaMatch, on_delete=models.CASCADE)
+	AWARDS = [("A", "award name"), ("B", "award name 2")]
 	award = models.CharField(choices=AWARDS, max_length = 60)
 

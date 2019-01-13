5969d8d81127b1862d8cd4a6b3d274472d9b19c9
Tratt <imoxus@gmail.com>
2019-01-13 01:48:47 +0000

    fix overlapping url names

--- tournamentstatus/urls.py
+++ tournamentstatus/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('', views.index, name='index'),
-	path('martian-enterprise', views.index, name='index'),
+	path('', views.index, name='tournamentstatus-root'),
+	path('martian-enterprise', views.index, name='martian-enterprise'),
 	path('test', views.mainpage, name="mainpage"),
 	path('test2', views.testpage, name="testpage"),
 	path('rules', views.rules, name="rules"),

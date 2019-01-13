a0a8010e5cfb38dea8d5c457ab334744b51dc657
Trattpingvin <imoxus@gmail.com>
2019-01-13 04:48:12 +0100

    add basic model interaction with views, and new template where it will be used

--- matchmaking/urls.py
+++ matchmaking/urls.py
@@ -4,4 +4,13 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('genffa', views.gen_ffa_matches, name='genffa'),
+	path('gen1v1', views.gen_1v1_matches, name='gen1v1'),
+	path('removematch/<int:match_id>', views.remove_match, name='removematch'),
+	path('removematch/', views.remove_match, name='removematch'),
+	path('inspectmatch/<int:match_id>', views.inspect_match, name='inspectmatch'),
+	path('inspectmatch/', views.inspect_match, name='inspectmatch'),
+	path('showmatch/<int:match_id>', views.show_match, name='showmatch'),
+	path('showmatch/', views.show_match, name='showmatch'),
+
 ]

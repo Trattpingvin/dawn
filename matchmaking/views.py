6a71b2e066ae10375575ef16849346ee2a70e421
Johan Eriksson <imoxus@gmail.com>
2019-01-14 08:45:25 +0100

    wip

--- matchmaking/views.py
+++ matchmaking/views.py
@@ -3,38 +3,41 @@ from django.http import HttpResponse
 import sys
 from django.contrib.admin.views.decorators import staff_member_required
 from matchmaking.models import *
+from django.views.generic import ListView, DeleteView
 import dawnotc.matchmaking
 
 
-@staff_member_required
-def index(request):
-	matches = FFaMatch.objects.all()
-	return render(request, "matchmaking.html", {"matchlist":matches})
+class MainView(ListView):
+	model = FFaMatch
 
-@staff_member_required
-def gen_ffa_matches(request):
-	amount = 5
-	all_players = Player.objects.all()
-	result = dawnotc.matchmaking.generate_matches(players)
-	return render(request, "matchmaking.html", {"matchlist":result})
+class GenMatchesView(View)
+	def post():
+		amount = 5
+		all_players = Player.objects.all()
+		result = dawnotc.matchmaking.generate_matches(players)
+		return render(request, "matchmaking.html", {"matchlist":result})
 
-@staff_member_required
-def gen_1v1_matches(request):
-	pass
+	def get():
+		return HttpResponse("How did this happen?")
 
-@staff_member_required
-def remove_match(request, match_id = None):
-	if not match_id:
-		return HttpResponse("no match given")
+class RemoveMatch(DeleteView):
+	model = FFaMatch
 
-@staff_member_required
-def show_match(request, match_id = None):
-	if not match_id:
-		return HttpResponse("no match given")
+class MatchView(View):
+	def get():
+		dummy_t1 = Team(name="t1")
+		dummy_t2 = Team(name="t2")
+		dummy_t3 = Team(name="t3")
+		dummy_t4 = Team(name="t4")
+		dummy_p1 = Player(name="p1", team=dummy_t1, bracket=2, stars=2, preference=0, availability=15)
+		dummy_p2 = Player(name="p2", team=dummy_t2, bracket=2, stars=2, preference=0, availability=15)
+		dummy_p3 = Player(name="p3", team=dummy_t3, bracket=2, stars=2, preference=0, availability=15)
+		dummy_p4 = Player(name="p4", team=dummy_t4, bracket=2, stars=2, preference=0, availability=15)
+		dummy_match = Match(player1=dummy_p1, player2=dummy_p2, player3=dummy_p3, player4=dummy_p4, location="M", winner=dummy_p2)
 
-@staff_member_required
-def inspect_match(request, match_id=None):
-	#POST view
+		return render(request, "matchmaking.html", {"matchlist":[dummy_match]})
+
+	def post():
 	if not match_id:
 		return HttpResponse("no match given")
 	result = FFaMatch.objects.filter(id=match_id)
--- tournamentstatus/urls.py
+++ tournamentstatus/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
+from django.views.generic.base import TemplateView
 
 from . import views
-
+TemplateView.as_view(template_name='')
 urlpatterns = [
-	path('', views.mainpage, name='tournamentstatus-root'),
-	path('martian-enterprise', views.mainpage, name='martian-enterprise'),
-	path('test2', views.testpage, name="testpage"),
-	path('martian-enterprise/rules', views.rules, name="rules"),
-	path('martian-enterprise/schedule', views.schedule, name="schedule"),
-	path('martian-enterprise/players', views.players, name="players"),
-	path('martian-enterprise/schedule/day1', views.day1 , name='day1'),
-	path('martian-enterprise/schedule/day2', views.day2 , name='day2'),
-	path('martian-enterprise/schedule/day3', views.day3 , name='day3'),
-	path('martian-enterprise/schedule/day4', views.day4 , name='day4'),
-	path('martian-enterprise/schedule/workshop', views.workshop, name='workshop'),
-	path('martian-enterprise/schedule/drafting', views.drafting, name='drafting'),
+	path('', TemplateView.as_view(template_name='main.html'), name='tournamentstatus-root'),
+	path('martian-enterprise', TemplateView.as_view(template_name='main.html'), name='martian-enterprise'),
+	path('martian-enterprise/rules', TemplateView.as_view(template_name='rules.html'), name="rules"),
+	path('martian-enterprise/schedule', TemplateView.as_view(template_name='schedule/schedule.html'), name="schedule"),
+	path('martian-enterprise/players', TemplateView.as_view(template_name='players.html'), name="players"),
+	path('martian-enterprise/schedule/day1', TemplateView.as_view(template_name='schedule/day1.html') , name='day1'),
+	path('martian-enterprise/schedule/day2', TemplateView.as_view(template_name='schedule/day2.html') , name='day2'),
+	path('martian-enterprise/schedule/day3', TemplateView.as_view(template_name='schedule/day3.html') , name='day3'),
+	path('martian-enterprise/schedule/day4', TemplateView.as_view(template_name='schedule/day4.html') , name='day4'),
+	path('martian-enterprise/schedule/workshop', TemplateView.as_view(template_name='schedule/workshop.html'), name='workshop'),
+	path('martian-enterprise/schedule/drafting', TemplateView.as_view(template_name='schedule/drafting.html'), name='drafting'),
 ]

0782bfeabb77895e25d3e101ec6f1fb681962b96
Johan Eriksson <imoxus@gmail.com>
2019-01-15 02:21:15 +0100

    converted tabs to spaces

--- matchmaking/models.py
+++ matchmaking/models.py
@@ -6,55 +6,55 @@ from django.core.exceptions import ValidationError
 def validate_preference(val):
 #preference is 3 bit value. bit order 0x1 = mars, 0x10 = ceres, 0x100 = io
 #bit 1 means veto
-	if val<0 or val>7:
-		raise ValidationError("%(val)s is not a valid preference", params={'value':val})
+    if val<0 or val>7:
+        raise ValidationError("%(val)s is not a valid preference", params={'value':val})
 
 def validate_availability(val):
 #availability is 4 bit value(one per day) bit order 0x1 = day 1, 0x10 = day 2, 0x100 = day 3, 0x1000 = day 4
 #bit 1 means available, 0 means unavailable
-	if val<0 or val>15:
-		raise ValidationError("%(val)s is not a valid availability", params={'value':val})
+    if val<0 or val>15:
+        raise ValidationError("%(val)s is not a valid availability", params={'value':val})
 
 class Team(models.Model):
-	name = models.CharField(max_length = 128)
+    name = models.CharField(max_length = 128)
 
 class Tournament(models.Model):
-	team1 = models.ForeignKey(Team, related_name="team1", on_delete=models.CASCADE)
-	team2 = models.ForeignKey(Team, related_name="team2", on_delete=models.CASCADE)
-	team3 = models.ForeignKey(Team, related_name="team3", on_delete=models.CASCADE)
-	team4 = models.ForeignKey(Team, related_name="team4", on_delete=models.CASCADE)
+    team1 = models.ForeignKey(Team, related_name="team1", on_delete=models.CASCADE)
+    team2 = models.ForeignKey(Team, related_name="team2", on_delete=models.CASCADE)
+    team3 = models.ForeignKey(Team, related_name="team3", on_delete=models.CASCADE)
+    team4 = models.ForeignKey(Team, related_name="team4", on_delete=models.CASCADE)
 
 class Player(models.Model):
-	name = models.CharField(primary_key=True, max_length = 40)
-	team = models.ForeignKey(Team, on_delete=models.CASCADE)
-	bracket = models.IntegerField()
-	stars = models.IntegerField()
-	preference = models.IntegerField(validators = [validate_preference])
-	availability = models.IntegerField(validators = [validate_availability])
+    name = models.CharField(primary_key=True, max_length = 40)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    bracket = models.IntegerField()
+    stars = models.IntegerField()
+    preference = models.IntegerField(validators = [validate_preference])
+    availability = models.IntegerField(validators = [validate_availability])
 
 class Match(models.Model):
-	day = models.IntegerField()
-	LOCATIONS = [("M","Mars"), ("C", "Ceres"), ("I", "Io")]
-	location = models.CharField(choices=LOCATIONS, max_length = 6)
-	winner = models.ForeignKey(Player, null=True, on_delete=models.CASCADE)
-	class Meta:
-		abstract=True
+    day = models.IntegerField()
+    LOCATIONS = [("M","Mars"), ("C", "Ceres"), ("I", "Io")]
+    location = models.CharField(choices=LOCATIONS, max_length = 6)
+    winner = models.ForeignKey(Player, null=True, on_delete=models.CASCADE)
+    class Meta:
+        abstract=True
 
 class OvOMatch(Match):
-	player1 = models.ForeignKey(Player, related_name="p1", on_delete=models.CASCADE)
-	player2 = models.ForeignKey(Player, related_name="p2", on_delete=models.CASCADE)
+    player1 = models.ForeignKey(Player, related_name="p1", on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Player, related_name="p2", on_delete=models.CASCADE)
 
 class FFaMatch(Match):
-	player1 = models.ForeignKey(Player, related_name="player1", on_delete=models.CASCADE)
-	player2 = models.ForeignKey(Player, related_name="player2", on_delete=models.CASCADE)
-	player3 = models.ForeignKey(Player, related_name="player3", on_delete=models.CASCADE)
-	player4 = models.ForeignKey(Player, related_name="player4", on_delete=models.CASCADE)
+    player1 = models.ForeignKey(Player, related_name="player1", on_delete=models.CASCADE)
+    player2 = models.ForeignKey(Player, related_name="player2", on_delete=models.CASCADE)
+    player3 = models.ForeignKey(Player, related_name="player3", on_delete=models.CASCADE)
+    player4 = models.ForeignKey(Player, related_name="player4", on_delete=models.CASCADE)
 
 class Award(models.Model):
-	player = models.ForeignKey(Player, on_delete=models.CASCADE)
-	match = models.ForeignKey(FFaMatch, on_delete=models.CASCADE)
-	AWARDS = [("A", "Keen eyes on their stuff"), ("B", "Outstanding Performances"),
-	("C", "Race for the initial markets"), ("D", "Eggs in multiple baskets"),
-	("E", "Commercial Meteorology Experiment"), ("F", "Mind Games")]
-	award = models.CharField(choices=AWARDS, max_length = 255)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    match = models.ForeignKey(FFaMatch, on_delete=models.CASCADE)
+    AWARDS = [("A", "Keen eyes on their stuff"), ("B", "Outstanding Performances"),
+    ("C", "Race for the initial markets"), ("D", "Eggs in multiple baskets"),
+    ("E", "Commercial Meteorology Experiment"), ("F", "Mind Games")]
+    award = models.CharField(choices=AWARDS, max_length = 255)
 
--- matchmaking/views.py
+++ matchmaking/views.py
@@ -9,48 +9,48 @@ import dawnotc.matchmaking as dm
 
 
 class MainView(View):
-	def get(self, request):
-		return render(request, 'matchmaking/matchmaking.html', {"object_list":FFaMatch.objects.all()})
+    def get(self, request):
+        return render(request, 'matchmaking/matchmaking.html', {"object_list":FFaMatch.objects.all()})
 
 class GenMatchesView(View):
-	def post(self, request):
-		#matchmaking method was designed with a different format in mind. let's convert to that format before
-		# using it
+    def post(self, request):
+        #matchmaking method was designed with a different format in mind. let's convert to that format before
+        # using it
         day = 1
-		amount = 5
+        amount = 5
         dawnplayers = []
-		for p in Player.objects.all():
+        for p in Player.objects.all():
             dmp = dm.Player(p.name, p.bracket, 0, p.team.id)
             dmp.availability = p.availability
             #TODO doesn't care about preference yet
             #TODO get matches assigned and played
             dawn.players.append(dmp)
-		result = dawnotc.matchmaking.generate_matches(dawnplayers)
-		for match in result:
-			import pdb
+        result = dawnotc.matchmaking.generate_matches(dawnplayers)
+        for match in result:
+            import pdb
             pdb.set_trace()
-		#return render(request, "matchmaking/matchmaking.html", {"object_list":result})
+        #return render(request, "matchmaking/matchmaking.html", {"object_list":result})
         return redirect('matchmaking-root')
 
-	def get(self, request):
-		return HttpResponse("How did this happen?")
+    def get(self, request):
+        return HttpResponse("How did this happen?")
 
 class RemoveMatch(DeleteView):
-	model = FFaMatch
+    model = FFaMatch
 
 class MatchView(View):
-	def get(self, request, match_id):
-		dummy_t1 = Team(name="t1")
-		dummy_t2 = Team(name="t2")
-		dummy_t3 = Team(name="t3")
-		dummy_t4 = Team(name="t4")
-		dummy_p1 = Player(name="p1", team=dummy_t1, bracket=2, stars=2, preference=0, availability=15)
-		dummy_p2 = Player(name="p2", team=dummy_t2, bracket=2, stars=2, preference=0, availability=15)
-		dummy_p3 = Player(name="p3", team=dummy_t3, bracket=2, stars=2, preference=0, availability=15)
-		dummy_p4 = Player(name="p4", team=dummy_t4, bracket=2, stars=2, preference=0, availability=15)
-		dummy_ffa_match = FFaMatch(player1=dummy_p1, player2=dummy_p2, player3=dummy_p3, player4=dummy_p4, location="M", winner=dummy_p2)
-		dummy_1v1_match = OvOMatch(player1=dummy_p1, player2=dummy_p2, location="M", winner=dummy_p2)
-		return render(request, "matchmaking/matchmaking.html", {"object_list":[dummy_ffa_match, dummy_1v1_match]})
+    def get(self, request, match_id):
+        dummy_t1 = Team(name="t1")
+        dummy_t2 = Team(name="t2")
+        dummy_t3 = Team(name="t3")
+        dummy_t4 = Team(name="t4")
+        dummy_p1 = Player(name="p1", team=dummy_t1, bracket=2, stars=2, preference=0, availability=15)
+        dummy_p2 = Player(name="p2", team=dummy_t2, bracket=2, stars=2, preference=0, availability=15)
+        dummy_p3 = Player(name="p3", team=dummy_t3, bracket=2, stars=2, preference=0, availability=15)
+        dummy_p4 = Player(name="p4", team=dummy_t4, bracket=2, stars=2, preference=0, availability=15)
+        dummy_ffa_match = FFaMatch(player1=dummy_p1, player2=dummy_p2, player3=dummy_p3, player4=dummy_p4, location="M", winner=dummy_p2)
+        dummy_1v1_match = OvOMatch(player1=dummy_p1, player2=dummy_p2, location="M", winner=dummy_p2)
+        return render(request, "matchmaking/matchmaking.html", {"object_list":[dummy_ffa_match, dummy_1v1_match]})
 
-	def post(self, request):
-		return HttpResponse("how did this happen?")
+    def post(self, request):
+        return HttpResponse("how did this happen?")

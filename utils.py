2a9214ce7675734f6320a3282e273ae53d4533b0
Trattpingvin <imoxus@gmail.com>
2019-01-17 19:07:21 +0100

    WIP utils

--- utils.py
+++ utils.py
@@ -3,5 +3,14 @@ from tournament.models import Player
 def populate_player_database(filename):
 	with open(filename) as f:
 		for line in f:
-			splitline = line.splitline()
-			Player(*splitline).save()#might need to grab the team reference from the database - #TODO test that
+
+			import pdb
+			pdb.set_trace()
+			stripline = line.strip()
+			splitline = stripline.split(' ')
+			p = Player(*splitline)
+			p.save()#might need to grab the team reference from the database - #TODO test that
+
+def populate_team_database():
+	teams = ["Asteroid Alpacas", "Belters", "Circus", "Donkeys"]
+	# TODO finish this

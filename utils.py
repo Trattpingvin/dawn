b3592fd246d630ae21525adb06ed4240f27963cc
Johan Eriksson <imoxus@gmail.com>
2019-01-19 20:32:26 +0100

    add all the players to players.csv

--- utils.py
+++ utils.py
@@ -18,3 +18,7 @@ def populate_team_database():
 	teams = ["Asteroid Alpacas", "Belters", "Circus", "Donkeys"]
 	for team in teams:
 		Team(name=team).save()
+
+def empty_player_database():
+	for p in Player.objects.all():
+		p.delete()

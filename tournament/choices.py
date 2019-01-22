5181d31f57f5e35fcdfc640dd069f2f411e10d2f
Johan Eriksson <imoxus@gmail.com>
2019-01-23 00:50:29 +0100

    add forgotten file

---
+++ tournament/choices.py
@@ -0,0 +1,3 @@
+AWARDS = [("A", "Keen Eyes on Their Stuff"), ("B", "Outstanding Performances"),
+         ("C", "Race for the Initial Markets"), ("D", "Eggs in Multiple Baskets"),
+         ("E", "Commercial Meteorology Experiment"), ("F", "Mind Games")]

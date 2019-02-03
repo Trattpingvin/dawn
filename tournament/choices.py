7651311f432e42a6366bc6bcec5e25ff447cfc5f
Johan Eriksson <imoxus@gmail.com>
2019-02-03 23:54:55 +0100

    add new award type

--- tournament/choices.py
+++ tournament/choices.py
@@ -1,3 +1,4 @@
 AWARDS = [("A", "Keen Eyes on Their Stuff"), ("B", "Outstanding Performances"),
-         ("C", "Race for the Initial Markets"), ("D", "Eggs in Multiple Baskets"),
-         ("E", "Commercial Meteorology Experiment"), ("F", "Mind Games")]
+          ("C", "Race for the Initial Markets"), ("D", "Eggs in Multiple Baskets"),
+          ("E", "Commercial Meteorology Experiment"), ("F", "Mind Games"),
+          ("G", "Most black market used")]

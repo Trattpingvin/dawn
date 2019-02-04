9dc21ec9e46e6ebf485fbac94426fb7f4af01342
Johan Eriksson <imoxus@gmail.com>
2019-02-04 16:43:23 +0100

    add publish action to match admin, and make the admin prettier

--- tournament/admin.py
+++ tournament/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
 from tournament.models import *
-#  TODO: add action to matches to publish them
+
+
+def publish_match(modeladmin, request, queryset):
+	queryset.update(published=True)
+
+
+publish_match.short_description = "Publish match on lineup page"
+
+
+class MatchAdmin(admin.ModelAdmin):
+	list_display = ['__str__', 'published']
+	ordering = ['-day', 'round']
+	actions = [publish_match]
+
+
 admin.site.register(Player)
 admin.site.register(Team)
-admin.site.register(Match)
+admin.site.register(Match, MatchAdmin)
 admin.site.register(Award)
 admin.site.register(MatchResult)
